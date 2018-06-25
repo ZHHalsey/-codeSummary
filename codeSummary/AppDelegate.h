5418cbd8c606a05e6b52126a8dbab485a7cf835a
klgzwork001 <739415161@qq.com>
2018-06-25 15:46:13 +0800

    first commit

---
+++ codeSummary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  codeSummary
+//
+//  Created by klgzwork001 on 2018/6/25.
+//  Copyright © 2018年 klgzwork001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ codeSummary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  codeSummary
+//
+//  Created by klgzwork001 on 2018/6/25.
+//  Copyright © 2018年 klgzwork001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
